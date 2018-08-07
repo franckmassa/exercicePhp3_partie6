57459590d3a83fd472b8f8ea157a644fb56021b0
massa <massafranck@gmail.com>
2018-08-07 22:26:53 +0200

    commentaire

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
   <p>
     <?php
-    //Teste que les paramètres de l'url existent
+    //Teste que les paramètres de l'url existent et affichage des dates
     if(isset($_GET['startDate']) && isset($_GET['endDate'])){
       echo $_GET['startDate'] . ' ' .$_GET['endDate'];
     }
