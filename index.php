0cd1ebbfea9f2130608572918a32d095040f2434
massa <massafranck@gmail.com>
2018-08-20 22:18:41 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice3</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Teste que les paramètres de l'url existent et affichage des dates
-    if(isset($_GET['startDate']) && isset($_GET['endDate'])){
-      echo $_GET['startDate'] . ' ' .$_GET['endDate'];
-    }
-    ?>
-  </p>
-</body>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 3</title>
+    </head>
+    <body>
+        <p>
+            <?php
+//Condition pour vérifier l'existence des paramètres
+            if (!empty($_GET['startDate']) && !empty($_GET['endDate'])) {
+                //Affichage des paramètres
+                echo $_GET['startDate'] . ' - ' . $_GET['endDate'];
+            } else {
+                echo 'Il manque un paramètre';
+            }
+            ?>
+        </p>
+    </body>
 </html>
